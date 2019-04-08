55804214ea99d7e688be4c091ab63b7c9b3e8dd9
wuhan <43577526+honorwh@users.noreply.github.com>
2019-04-09 00:56:15 +0800

    Update 两数之和.py

--- python/Topics/数组/两数之和/两数之和.py
+++ python/Topics/数组/两数之和/两数之和.py
@@ -10,7 +10,7 @@ class Solution:
         while size < len(nums):
             if target-nums[size] in d:
                 if d[target-nums[size]] < size:
-                    return [d[target-nums[size]],size]
+                    return [d[target-nums[size]],size]  #因为题目已经假设了每种输入只会对应一个答案
             else:
                 d[nums[size]] = size
             size = size + 1
